7b20f9567c02f894db98006473790921a9011faf
nofarisraeli <nofarisraeli7@gmail.com>
2018-08-30 11:35:51 +0300

    Registration
    Add 2 models and controller for signIn and logIn to the website

---
+++ Lowtel/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Lowtel.Controllers
+
+{
+    internal class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Lowtel/Models/RegisterViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lowtel.Models
+{
+    public class RegisterViewModel
+    {
+        [Required, MaxLength(20)]
+        public string FirstName { get; set; }
+
+        [Required, MaxLength(20)]
+        public string LastName { get; set; }
+
+        [Required, MaxLength(20)]
+        public string Email { get; set; }
+
+        [Required, DataType(DataType.Password),MinLength(6),MaxLength(10)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password), Compare(nameof(Password),ErrorMessage = "The password does not match the confirmation password")]
+        public string ConfirmPassword { get; set; }
+    }
+}

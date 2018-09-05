5448475bc4c96e871460d75b64d8f89fe8a6fc4a
yonathantzur1 <yonathantzur1@gmail.com>
2018-09-05 20:34:52 +0300

    Minor bug fixes to users login

--- Lowtel/Controllers/UsersController.cs
+++ Lowtel/Controllers/UsersController.cs
@@ -13,9 +13,14 @@ namespace Lowtel.Controllers
 {
     public class UsersController : Controller
     {
+        public struct UserResult
+        {            
+            public bool isLogin { get; set; }
+            public string userName { get; set; }
+        };
+
         public const string SessionName = "UserName";     
-        private readonly LotelContext _context;
-        public static bool isLogin = false;
+        private readonly LotelContext _context;        
         public static string userName = "";
 
         public UsersController(LotelContext context)
@@ -25,8 +30,15 @@ namespace Lowtel.Controllers
 
         // GET: Users
         public async Task<IActionResult> Index()
-        {
-            return View(await _context.User.ToListAsync());
+        {            
+            if (checkSession().isLogin)
+            {
+                return View(await _context.User.ToListAsync());
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }            
         }
 
         // GET: Users/Details/5
@@ -106,7 +118,7 @@ namespace Lowtel.Controllers
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!UserExists(user.UserName))
+                    if (!UserExists(user))
                     {
                         return NotFound();
                     }
@@ -149,9 +161,9 @@ namespace Lowtel.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-        private bool UserExists(string id)
+        private bool UserExists(User user)
         {
-            return _context.User.Any(e => e.UserName == id);
+            return _context.User.Any(u => (u.UserName == user.UserName) && (u.Password == user.Password));
         }
 
         // This function return the view of login page
@@ -167,14 +179,13 @@ namespace Lowtel.Controllers
         {
             if (ModelState.IsValid)
             {
-                if (UserExists(user.UserName))
+                if (UserExists(user))
                 {
-                    HttpContext.Session.SetString(SessionName, user.UserName);
-                    isLogin = true;
+                    HttpContext.Session.SetString(SessionName, user.UserName);                    
                     return RedirectToAction("Index", "Home");
                 }
             }
-            isLogin = false;
+            
             return NotFound();
         }
 
@@ -182,36 +193,30 @@ namespace Lowtel.Controllers
         // return: redirection to homePage
         public IActionResult LogOff()
         {
-
-            HttpContext.Session.Clear();
-            isLogin = false;
+            HttpContext.Session.Clear();            
             return RedirectToAction("Index", "Home");
         }
 
         // This function check if there is a user who has already logged in
         // return : json of array with userName and isLogin boolean
         [HttpGet]
-        public JsonResult checkSession()
+        public UserResult checkSession()
         {
-            var userName = HttpContext.Session.GetString(UsersController.SessionName);
-            List<dynamic> data = new List<dynamic>();
-            data.Add(userName);
+            string userName = HttpContext.Session.GetString(UsersController.SessionName);            
+            UserResult res = new UserResult();
 
             if (userName != null)
             {
-                isLogin = true;
+                res.isLogin = true;
+                res.userName = userName;
             }
             else
             {
-                isLogin = false;
-
+                res.isLogin = false;
             }
-            data.Add(isLogin);
-            return Json(new
-            {
-                isLogin = isLogin,
-                userName = userName
-            });
+
+
+            return(res);
         }
 
     }
